4517fce2f774bba7e34ad4a8982859677adb785d
Enzo <ochotecoenzo@yahoo.com.ar>
2026-01-16 09:43:50 -0300

    arreglando casing 2

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express()
 
 app.use(cors({
   origin: ["http://localhost:5173",
-    "https://elfrontend.com"
+    "https://time-management-app-three.vercel.app"
   ],
   credentials: true
 }));
